e8a9bc9bc331847410b17711631637cba838ecb4
Yuwang Cai <mrcaidev@gmail.com>
2025-04-07 21:07:37 +0800

    fix: missing expo module

--- patient-app/src/app/_layout.tsx
+++ patient-app/src/app/_layout.tsx
@@ -1,11 +1,5 @@
 import { Slot } from "expo-router";
-import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  return (
-    <>
-      <StatusBar />
-      <Slot />
-    </>
-  );
+  return <Slot />;
 }
