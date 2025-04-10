f99b36a95147ecabd556c7c428942435c396e956
Yuwang Cai <mrcaidev@gmail.com>
2025-04-10 22:39:16 +0800

    feat: default error alert

--- patient-app/src/providers/query.tsx
+++ patient-app/src/providers/query.tsx
@@ -1,7 +1,16 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { PropsWithChildren } from "react";
+import { Alert } from "react-native";
 
-const client = new QueryClient();
+const client = new QueryClient({
+  defaultOptions: {
+    mutations: {
+      onError: (error) => {
+        Alert.alert("Error", error.message, [{ text: "OK" }]);
+      },
+    },
+  },
+});
 
 export function QueryProvider({ children }: PropsWithChildren) {
   return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
