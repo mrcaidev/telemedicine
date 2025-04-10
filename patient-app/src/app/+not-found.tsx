b365ecb4816b7116899a0a5c99115262f41fe0bc
Yuwang Cai <mrcaidev@gmail.com>
2025-04-11 00:01:48 +0800

    feat: 404 page

---
+++ patient-app/src/app/+not-found.tsx
@@ -0,0 +1,37 @@
+import { useRouter } from "expo-router";
+import { View } from "react-native";
+import { Button, Text } from "react-native-paper";
+
+export default function NotFoundPage() {
+  const router = useRouter();
+
+  const goHome = () => {
+    router.push("/");
+  };
+
+  return (
+    <View
+      style={{
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 24,
+      }}
+    >
+      <Text style={{ fontSize: 112, fontWeight: "bold" }}>404</Text>
+      <Text style={{ marginBottom: 16, fontSize: 20, fontWeight: "medium" }}>
+        Oops! Page not found
+      </Text>
+      <Text
+        style={{ marginBottom: 24, textAlign: "center" }}
+        className="mb-6 text-muted-foreground text-center text-balance"
+      >
+        It seems like the page you are looking for does not exist or might have
+        been removed.
+      </Text>
+      <Button mode="contained" icon="home" onPress={goHome}>
+        Take me home
+      </Button>
+    </View>
+  );
+}
