39a89708d9330e8998b8b5e4932573b6a1234b7e
Yuwang Cai <mrcaidev@gmail.com>
2025-04-12 14:24:28 +0800

    feat: loading and error state of upcoming appointment

--- patient-app/src/app/(auth)/(tabs)/index.tsx
+++ patient-app/src/app/(auth)/(tabs)/index.tsx
@@ -1,30 +1,35 @@
 import { useUpcomingAppointmentQuery } from "@/api/appointment";
-import { HighlightedAppointment } from "@/components/appointment/highlighted-appointment";
+import {
+  HighlightedAppointment,
+  HighlightedAppointmentError,
+  HighlightedAppointmentSkeleton,
+} from "@/components/appointment/highlighted-appointment";
 import { ScrollView, View } from "react-native";
 import { Text } from "react-native-paper";
 
 export default function HomePage() {
   return (
     <ScrollView style={{ paddingHorizontal: 24 }}>
-      <UpcomingAppointment />
+      <View style={{ paddingVertical: 24 }}>
+        <Text style={{ marginBottom: 16, fontSize: 18, fontWeight: "bold" }}>
+          Upcoming Appointment
+        </Text>
+        <UpcomingAppointment />
+      </View>
     </ScrollView>
   );
 }
 
 function UpcomingAppointment() {
-  const { data: appointment, error, isPending } = useUpcomingAppointmentQuery();
+  const { data, error, isPending } = useUpcomingAppointmentQuery();
 
   if (isPending) {
-    return <Text>Skeleton</Text>;
+    return <HighlightedAppointmentSkeleton />;
   }
 
   if (error) {
-    return (
-      <View>
-        <Text>{error.message}</Text>
-      </View>
-    );
+    return <HighlightedAppointmentError message={error.message} />;
   }
 
-  return <HighlightedAppointment appointment={appointment} />;
+  return <HighlightedAppointment data={data} />;
 }
--- patient-app/src/components/appointment/highlighted-appointment.tsx
+++ patient-app/src/components/appointment/highlighted-appointment.tsx
@@ -3,85 +3,128 @@ import { Link } from "expo-router";
 import { View } from "react-native";
 import { Icon, Surface, Text, useTheme } from "react-native-paper";
 import { Avatar } from "../avatar";
+import { Skeleton } from "../skeleton";
 import { StatusBadge } from "./status-badge";
 
 type Props = {
-  appointment: Appointment;
+  data: Appointment;
 };
 
-export function HighlightedAppointment({ appointment }: Props) {
-  const { id, doctor, date, startTime, endTime } = appointment;
+export function HighlightedAppointment({ data }: Props) {
+  const { id, doctor, date, startTime, endTime } = data;
 
   const theme = useTheme();
 
   return (
-    <View style={{ paddingVertical: 24 }}>
-      <Text style={{ marginBottom: 16, fontSize: 18, fontWeight: "bold" }}>
-        Upcoming Appointment
-      </Text>
-      <Link href={{ pathname: "/appointment/[id]", params: { id } }}>
-        <Surface style={{ width: "100%", padding: 16, borderRadius: 8 }}>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 10,
-              marginBottom: 16,
-            }}
-          >
-            <Avatar
-              url={doctor.avatarUrl}
-              text={`${doctor.firstName[0]}${doctor.lastName[0]}`}
-              size={44}
-            />
-            <View style={{ marginRight: "auto" }}>
-              <Text numberOfLines={1} style={{ maxWidth: 128, fontSize: 16 }}>
-                Dr. {doctor.firstName} {doctor.lastName}
-              </Text>
-              <Text style={{ opacity: 0.7 }}>{doctor.specialties[0]}</Text>
-            </View>
-            <StatusBadge appointment={appointment} />
-          </View>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 8,
-              marginBottom: 12,
-            }}
-          >
-            <Icon source="calendar-clock-outline" size={16} />
-            <Text style={{ lineHeight: 16 }}>
-              {new Date(date).toLocaleDateString("en-SG", {
-                weekday: "long",
-                month: "long",
-                day: "numeric",
-              })}
-              &nbsp;
-              <Text style={{ fontWeight: "bold" }}>·</Text>
-              &nbsp;
-              {new Date(`${date} ${startTime}`).toLocaleTimeString("en-SG", {
-                hour: "numeric",
-                minute: "numeric",
-              })}
-              &nbsp;-&nbsp;
-              {new Date(`${date} ${endTime}`).toLocaleTimeString("en-SG", {
-                hour: "numeric",
-                minute: "numeric",
-              })}
+    <Link href={{ pathname: "/appointment/[id]", params: { id } }}>
+      <Surface style={{ width: "100%", padding: 16, borderRadius: 8 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 10,
+            marginBottom: 16,
+          }}
+        >
+          <Avatar
+            url={doctor.avatarUrl}
+            text={`${doctor.firstName[0]}${doctor.lastName[0]}`}
+            size={44}
+          />
+          <View style={{ marginRight: "auto" }}>
+            <Text numberOfLines={1} style={{ maxWidth: 128, fontSize: 16 }}>
+              Dr. {doctor.firstName} {doctor.lastName}
             </Text>
+            <Text style={{ opacity: 0.7 }}>{doctor.specialties[0]}</Text>
           </View>
-          <Text
-            style={{
-              alignSelf: "flex-end",
-              color: theme.colors.primary,
-              fontSize: 12,
-            }}
-          >
-            View details &gt;
+          <StatusBadge appointment={data} />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 8,
+            marginBottom: 12,
+          }}
+        >
+          <Icon source="calendar-clock-outline" size={16} />
+          <Text style={{ lineHeight: 16 }}>
+            {new Date(date).toLocaleDateString("en-SG", {
+              weekday: "long",
+              month: "long",
+              day: "numeric",
+            })}
+            &nbsp;
+            <Text style={{ fontWeight: "bold" }}>·</Text>
+            &nbsp;
+            {new Date(`${date} ${startTime}`).toLocaleTimeString("en-SG", {
+              hour: "numeric",
+              minute: "numeric",
+            })}
+            &nbsp;-&nbsp;
+            {new Date(`${date} ${endTime}`).toLocaleTimeString("en-SG", {
+              hour: "numeric",
+              minute: "numeric",
+            })}
           </Text>
-        </Surface>
-      </Link>
-    </View>
+        </View>
+        <Text
+          style={{
+            alignSelf: "flex-end",
+            color: theme.colors.primary,
+            fontSize: 12,
+          }}
+        >
+          View details &gt;
+        </Text>
+      </Surface>
+    </Link>
+  );
+}
+
+export function HighlightedAppointmentSkeleton() {
+  return (
+    <Surface style={{ padding: 16, borderRadius: 8 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 10,
+          marginBottom: 16,
+        }}
+      >
+        <Skeleton style={{ width: 44, height: 44, borderRadius: "50%" }} />
+        <View style={{ gap: 4, marginRight: "auto" }}>
+          <Skeleton style={{ width: 120, height: 20 }} />
+          <Skeleton style={{ width: 80, height: 16 }} />
+        </View>
+        <Skeleton style={{ width: 80, height: 24 }} />
+      </View>
+      <Skeleton style={{ width: "100%", height: 16, marginBottom: 12 }} />
+      <Skeleton style={{ alignSelf: "flex-end", width: 80, height: 16 }} />
+    </Surface>
+  );
+}
+
+type ErrorProps = {
+  message: string;
+};
+
+export function HighlightedAppointmentError({ message }: ErrorProps) {
+  return (
+    <Surface
+      style={{
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 6,
+        height: 136,
+        padding: 16,
+        borderRadius: 8,
+      }}
+    >
+      <Icon source="alert-circle-outline" size={16} />
+      <Text>{message}</Text>
+    </Surface>
   );
 }
