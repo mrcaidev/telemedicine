5c7f7b14458924c02a255e17943cd5e385f6beee
Yuwang Cai <mrcaidev@gmail.com>
2025-04-12 14:52:39 +0800

    feat: view all link besides upcoming appointment title

--- patient-app/src/app/(auth)/(tabs)/index.tsx
+++ patient-app/src/app/(auth)/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import {
   HighlightedAppointmentError,
   HighlightedAppointmentSkeleton,
 } from "@/components/appointment/highlighted-appointment";
+import { Link } from "expo-router";
 import { ScrollView, View } from "react-native";
 import { Text } from "react-native-paper";
 
@@ -11,9 +12,21 @@ export default function HomePage() {
   return (
     <ScrollView style={{ paddingHorizontal: 24 }}>
       <View style={{ paddingVertical: 24 }}>
-        <Text style={{ marginBottom: 16, fontSize: 18, fontWeight: "bold" }}>
-          Upcoming Appointment
-        </Text>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginBottom: 16,
+          }}
+        >
+          <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+            Upcoming Appointment
+          </Text>
+          <Link href="/appointment" style={{ fontSize: 14 }}>
+            View all
+          </Link>
+        </View>
         <UpcomingAppointment />
       </View>
     </ScrollView>
