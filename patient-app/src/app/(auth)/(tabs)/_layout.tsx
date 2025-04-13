3ce3d0ad1f8081310ad5698d67ff4b12552587af
Yuwang Cai <mrcaidev@gmail.com>
2025-04-13 20:01:03 +0800

    fix: theme color in tab bar

--- patient-app/src/app/(auth)/(tabs)/_layout.tsx
+++ patient-app/src/app/(auth)/(tabs)/_layout.tsx
@@ -4,13 +4,27 @@ import { CalendarIcon, HouseIcon, UserRoundIcon } from "lucide-react-native";
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ animation: "shift", headerShown: false }}>
+    <Tabs
+      screenOptions={{
+        animation: "shift",
+        headerShown: false,
+        tabBarActiveTintColor: "#009689",
+        tabBarInactiveTintColor: "#71717a",
+        sceneStyle: {
+          backgroundColor: "transparent",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           tabBarLabel: "Home",
-          tabBarIcon: ({ color, size }) => (
-            <Icon as={HouseIcon} color={color} size={size} />
+          tabBarIcon: ({ focused, size }) => (
+            <Icon
+              as={HouseIcon}
+              size={size}
+              className={focused ? "text-primary" : "text-muted-foreground"}
+            />
           ),
         }}
       />
@@ -18,8 +32,12 @@ export default function TabsLayout() {
         name="appointment"
         options={{
           tabBarLabel: "Appointment",
-          tabBarIcon: ({ color, size }) => (
-            <Icon as={CalendarIcon} color={color} size={size} />
+          tabBarIcon: ({ focused, size }) => (
+            <Icon
+              as={CalendarIcon}
+              size={size}
+              className={focused ? "text-primary" : "text-muted-foreground"}
+            />
           ),
         }}
       />
@@ -27,8 +45,12 @@ export default function TabsLayout() {
         name="me"
         options={{
           tabBarLabel: "Me",
-          tabBarIcon: ({ color, size }) => (
-            <Icon as={UserRoundIcon} color={color} size={size} />
+          tabBarIcon: ({ focused, size }) => (
+            <Icon
+              as={UserRoundIcon}
+              size={size}
+              className={focused ? "text-primary" : "text-muted-foreground"}
+            />
           ),
         }}
       />
