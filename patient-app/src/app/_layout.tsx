a1e410c6b9812ac08e34dcdc93bfe161e3dd73fb
Yuwang Cai <mrcaidev@gmail.com>
2025-04-21 14:49:42 +0800

    chore: do not need theme provider

--- patient-app/src/app/_layout.tsx
+++ patient-app/src/app/_layout.tsx
@@ -1,17 +1,14 @@
 import "@/global.css";
 import { QueryProvider } from "@/providers/query";
-import { ThemeProvider } from "@/providers/theme";
 import "@/utils/datetime";
 import { PortalHost } from "@rn-primitives/portal";
 import { Slot } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <ThemeProvider>
-      <QueryProvider>
-        <Slot />
-        <PortalHost />
-      </QueryProvider>
-    </ThemeProvider>
+    <QueryProvider>
+      <Slot />
+      <PortalHost />
+    </QueryProvider>
   );
 }
