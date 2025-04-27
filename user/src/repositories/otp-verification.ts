571fb6adb35f5e099c93fa1ae05ddfce5997a3f2
Yuwang Cai <mrcaidev@gmail.com>
2025-04-27 19:21:17 +0800

    refactor: otp

--- user/src/repositories/otp-verification.ts
+++ user/src/repositories/otp-verification.ts
@@ -3,33 +3,48 @@ import type { OtpVerification } from "@/utils/types";
 import { sql } from "bun";
 
 export async function findLastOneByEmail(email: string) {
-  const [otpVerification] = await sql`
-    select *
+  const [row] = await sql`
+    select id, email, otp, sent_at, verified_at
     from otp_verifications
     where email = ${email}
     order by sent_at desc
     limit 1
   `;
-  return snakeToCamelJson(otpVerification) as OtpVerification | undefined;
+
+  if (!row) {
+    return null;
+  }
+
+  return snakeToCamelJson(row) as OtpVerification;
 }
 
 export async function insertOne(data: Pick<OtpVerification, "email" | "otp">) {
-  const [otpVerification] = await sql`
+  const [row] = await sql`
     insert into otp_verifications ${sql(camelToSnakeJson(data))}
-    returning *
+    returning id, email, otp, sent_at, verified_at
   `;
-  return snakeToCamelJson(otpVerification) as OtpVerification;
+
+  if (!row) {
+    throw new Error("failed to insert OTP verification");
+  }
+
+  return snakeToCamelJson(row) as OtpVerification;
 }
 
 export async function updateOneById(
   id: string,
   data: Partial<Pick<OtpVerification, "verifiedAt">>,
 ) {
-  const [otpVerification] = await sql`
+  const [row] = await sql`
     update otp_verifications
     set ${sql(camelToSnakeJson(data))}
     where id = ${id}
-    returning *
+    returning id, email, otp, sent_at, verified_at
   `;
-  return snakeToCamelJson(otpVerification) as OtpVerification | undefined;
+
+  if (!row) {
+    throw new Error("failed to update OTP verification");
+  }
+
+  return snakeToCamelJson(row) as OtpVerification;
 }
