9a9da8e18f01906f2e706c4225a51d91a35a50e0
Yuwang Cai <mrcaidev@gmail.com>
2025-04-27 18:16:28 +0800

    refactor: no need for onError

--- notification/src/index.ts
+++ notification/src/index.ts
@@ -1,6 +1,5 @@
 import "@/events/consumer";
 import { Hono } from "hono";
-import { HTTPException } from "hono/http-exception";
 import { logger } from "hono/logger";
 
 const app = new Hono();
@@ -13,18 +12,6 @@ app.get("/livez", (c) => {
 
 app.get("/readyz", async (c) => {
   return c.text("ready");
-});
-
-app.onError((error, c) => {
-  if (error instanceof HTTPException) {
-    return c.json(
-      { code: error.status, message: error.message, data: null },
-      error.status,
-    );
-  }
-
-  console.error(error);
-  return c.json({ code: 500, message: "Unknown error", data: null }, 500);
 });
 
 export default app;
