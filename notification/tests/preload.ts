2408b86141cd5275d07c85e21d9f01e753cc5207
Yuwang Cai <mrcaidev@gmail.com>
2025-04-30 00:17:06 +0800

    test: setup notification tests

---
+++ notification/tests/preload.ts
@@ -0,0 +1,27 @@
+import { mock } from "bun:test";
+
+mock.module("kafkajs", () => ({
+  Kafka: class {
+    consumer() {
+      return {
+        connect: () => {},
+        disconnect: () => {},
+        subscribe: () => {},
+        run: () => {},
+      };
+    }
+  },
+  logLevel: {
+    ERROR: 1,
+  },
+}));
+
+mock.module("resend", () => ({
+  Resend: class {
+    email = new (class {
+      send() {}
+      update() {}
+      cancel() {}
+    })();
+  },
+}));
