985da5dc5b81d8064769d0d1b73808051266a55e
Yuwang Cai <mrcaidev@gmail.com>
2025-04-29 23:58:56 +0800

    style: keep logs lowercase

--- notification/src/utils/resend.ts
+++ notification/src/utils/resend.ts
@@ -5,5 +5,5 @@ export const resend = new Resend(Bun.env.RESEND_API_KEY);
 export const SHOULD_REALLY_SEND = Bun.env.RESEND_API_KEY.startsWith("re_");
 
 if (!SHOULD_REALLY_SEND) {
-  console.log("Resend API key is invalid. Emails will not be sent.");
+  console.log("resend api key is invalid. emails will not be sent.");
 }
