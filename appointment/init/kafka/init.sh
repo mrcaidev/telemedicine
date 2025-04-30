843c39f9b33cc9320c546854bd7abea8f52a1672
Yuwang Cai <mrcaidev@gmail.com>
2025-04-30 13:12:04 +0800

    refactor: config files

---
+++ appointment/init/kafka/init.sh
@@ -0,0 +1,4 @@
+kafka-topics.sh --bootstrap-server kafka:9092 --create --topic PatientCreated
+kafka-topics.sh --bootstrap-server kafka:9092 --create --topic PatientUpdated
+kafka-topics.sh --bootstrap-server kafka:9092 --create --topic DoctorCreated
+kafka-topics.sh --bootstrap-server kafka:9092 --create --topic DoctorUpdated
