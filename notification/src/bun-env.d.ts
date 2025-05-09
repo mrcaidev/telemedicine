842fb8e4cb75e268826df2661311c10a31c9c2b9
Yuwang Cai <mrcaidev@gmail.com>
2025-05-09 16:12:31 +0800

    fix: sprint 2 endgame (#75)

--- notification/src/bun-env.d.ts
+++ notification/src/bun-env.d.ts
@@ -2,5 +2,11 @@ declare module "bun" {
   interface Env {
     RESEND_API_KEY: string;
     KAFKA_BROKERS: string;
+    KAFKA_SECURITY_PROTOCOL?: string;
+    KAKFA_SASL_USERNAME?: string;
+    KAFKA_SASL_PASSWORD?: string;
+    KAFKA_SSL_CA?: string;
+    KAFKA_SSL_KEY?: string;
+    KAFKA_SSL_CERT?: string;
   }
 }
---
+++ start.sh
@@ -0,0 +1,41 @@
+TAG=$(git rev-parse HEAD)
+echo "📌 Pinned version to $TAG"
+
+echo "⚙️ Configuring Helm..."
+helm repo add traefik https://traefik.github.io/charts
+helm repo add bitnami https://charts.bitnami.com/bitnami
+
+echo "🚀 Deploying traefik..."
+helm install traefik traefik/traefik --values ./deployment/traefik/values.yaml
+
+echo "🚀 Deploying kafka..."
+helm install kafka bitnami/kafka --values ./deployment/kafka/values.yaml
+
+echo "📦 Building notification..."
+docker build -t mrcaidev/telemedicine-notification:$TAG ./notification
+
+echo "🚀 Deploying notification..."
+minikube image load mrcaidev/telemedicine-notification:$TAG
+helm install notification ./deployment/notification --set image=mrcaidev/telemedicine-notification:$TAG
+
+echo "🚀 Deploying user-postgres..."
+kubectl create cm user-postgres-initdb --from-file=./user/init/postgres
+helm install user-postgres bitnami/postgresql --values ./deployment/user-postgres/values.yaml
+
+echo "📦 Building user..."
+docker build -t mrcaidev/telemedicine-user:$TAG ./user
+
+echo "🚀 Deploying user..."
+minikube image load mrcaidev/telemedicine-user:$TAG
+helm install user ./deployment/user --set image=mrcaidev/telemedicine-user:$TAG
+
+echo "🚀 Deploying appointment-postgres..."
+kubectl create cm appointment-postgres-initdb --from-file=./appointment/init/postgres
+helm install appointment-postgres bitnami/postgresql --values ./deployment/appointment-postgres/values.yaml
+
+echo "📦 Building appointment..."
+docker build -t mrcaidev/telemedicine-appointment:$TAG ./appointment
+
+echo "🚀 Deploying appointment..."
+minikube image load mrcaidev/telemedicine-appointment:$TAG
+helm install appointment ./deployment/appointment --set image=mrcaidev/telemedicine-appointment:$TAG
