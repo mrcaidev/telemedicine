ff1ca99b539335b675e6c4d4246bc6537cae9686
Yuwang Cai <mrcaidev@gmail.com>
2025-05-24 09:39:54 +0800

    refactor: code

--- auth-gateway/src/index.ts
+++ auth-gateway/src/index.ts
@@ -16,14 +16,17 @@ const JWT_SECRET = Bun.env.JWT_SECRET;
 app.get("/auth", async (c) => {
   try {
     const token = c.req.header("Authorization")?.slice(7);
+
     if (!token) {
       return c.body(null, 204);
     }
 
     const { id, role, email } = await verify(token, JWT_SECRET);
+
     c.header("X-User-Id", id as string);
     c.header("X-User-Role", role as string);
     c.header("X-User-Email", email as string);
+
     return c.body(null, 204);
   } catch {
     return c.body(null, 204);
