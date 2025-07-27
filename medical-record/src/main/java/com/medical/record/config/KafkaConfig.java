ba7d14678ba24c5860aec5fd373f8237d0163aa9
Haiming <1411843070@qq.com>
2025-07-27 14:58:35 +0800

    fix: 替换为yaml文件

--- medical-record/src/main/java/com/medical/record/config/KafkaConfig.java
+++ medical-record/src/main/java/com/medical/record/config/KafkaConfig.java
@@ -29,15 +29,6 @@ public class KafkaConfig {
         configProps.put("bootstrap.servers", bootstrapServers);
         configProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         configProps.put("value.serializer", JsonSerializer.class);
-        configProps.put("security.protocol", "SSL");
-// Truststore (CA)
-        configProps.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, "PEM");
-        configProps.put(SslConfigs.SSL_TRUSTSTORE_CERTIFICATES_CONFIG, "/etc/kafka/certs/ca.crt");
-
-// Keystore (client cert + key)
-        configProps.put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "PEM");
-        configProps.put(SslConfigs.SSL_KEYSTORE_CERTIFICATE_CHAIN_CONFIG, "/etc/kafka/certs/client.crt");
-        configProps.put(SslConfigs.SSL_KEYSTORE_KEY_CONFIG, "/etc/kafka/certs/client.key");
 
         // 配置JsonSerializer以支持对象序列化
 //        configProps.put(JsonSerializer.TYPE_MAPPINGS,
