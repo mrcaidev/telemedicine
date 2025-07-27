86721db762e1d6c0092cdec55abdd21c0885bc28
Haiming <1411843070@qq.com>
2025-07-27 15:11:00 +0800

    fix: 改回config

--- medical-record/src/main/java/com/medical/record/config/KafkaConfig.java
+++ medical-record/src/main/java/com/medical/record/config/KafkaConfig.java
@@ -30,6 +30,16 @@ public class KafkaConfig {
         configProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         configProps.put("value.serializer", JsonSerializer.class);
 
+        // SSL配置 - 使用PEM格式证书的正确配置方式
+        configProps.put("security.protocol", "SSL");
+        // 信任证书配置
+        configProps.put("ssl.truststore.type", "PEM");
+        configProps.put("ssl.truststore.certificates", "/etc/kafka/certs/ca.crt");
+        // 客户端证书和私钥配置
+        configProps.put("ssl.keystore.type", "PKCS12");
+        configProps.put("ssl.keystore.certificate.chain", "/etc/kafka/certs/client.crt");
+        configProps.put("ssl.keystore.key", "/etc/kafka/certs/client.key");
+
         // 配置JsonSerializer以支持对象序列化
 //        configProps.put(JsonSerializer.TYPE_MAPPINGS,
 //                "medical-record-created-event:com.medical.record.model.dto.MedicalRecordCreatedEvent");
