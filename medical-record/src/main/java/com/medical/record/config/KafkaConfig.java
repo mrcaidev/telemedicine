aba69405ac27d024d585c119c077e2f299adc6c2
Haiming <1411843070@qq.com>
2025-07-27 15:46:41 +0800

    fix: 直接读取

--- medical-record/src/main/java/com/medical/record/config/KafkaConfig.java
+++ medical-record/src/main/java/com/medical/record/config/KafkaConfig.java
@@ -11,6 +11,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,17 +37,25 @@ public class KafkaConfig {
         // SSL配置 - 使用PEM格式证书的正确配置方式
         configProps.put("security.protocol", "SSL");
         // 信任证书配置
-        configProps.put("ssl.truststore.type", "PEM");
-        configProps.put("ssl.truststore.certificates", "/etc/kafka/certs/ca.crt");
+        configProps.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, "PEM");
+        configProps.put(SslConfigs.SSL_TRUSTSTORE_CERTIFICATES_CONFIG, readCertificateFile("/etc/kafka/certs/ca.crt"));
         // 客户端证书和私钥配置
-        configProps.put("ssl.keystore.type", "PEM");
-        configProps.put("ssl.keystore.certificate.chain", "/etc/kafka/certs/client.crt");
-        configProps.put("ssl.keystore.key", "/etc/kafka/certs/client.key");
+        configProps.put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "PEM");
+        configProps.put(SslConfigs.SSL_KEYSTORE_CERTIFICATE_CHAIN_CONFIG, readCertificateFile(("/etc/kafka/certs/client.crt")));
+        configProps.put(SslConfigs.SSL_KEYSTORE_KEY_CONFIG, readCertificateFile("/etc/kafka/certs/client.key"));
 
         // 配置JsonSerializer以支持对象序列化
 //        configProps.put(JsonSerializer.TYPE_MAPPINGS,
 //                "medical-record-created-event:com.medical.record.model.dto.MedicalRecordCreatedEvent");
         return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    private String readCertificateFile(String filePath) {
+        try {
+            return Files.readString(Paths.get(filePath), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read certificate file: " + filePath, e);
+        }
     }
 
     @Bean
